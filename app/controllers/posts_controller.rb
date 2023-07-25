6c56876f091b6ad05d8866dd00942c24b4585044
kazu0412 <75778286+kazu0429@users.noreply.github.com>
2023-07-25 15:28:31 +0900

    add posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,30 @@
 class PostsController < ApplicationController
     def index
-        
+        # インスタンス
+        @posts = Post.all
     end
+
+    def new
+        # オブジェクト生成
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+
+        if @post.save
+            # /postsへリダイレクト
+            redirect_to posts_path
+        else
+            render:new
+        end
+    end
+    
+    # post_paramsへの修飾
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :content)
+    end
+
 end
