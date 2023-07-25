ae5bff9a191e1e2993aee0216e302b6adcb19d46
kazu0412 <75778286+kazu0429@users.noreply.github.com>
2023-07-25 16:09:14 +0900

    add edit, update, delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+
+    before_action :set_post, only: [:edit, :update, :destroy]
+
     def index
         # インスタンス
         @posts = Post.all
@@ -19,12 +22,34 @@ class PostsController < ApplicationController
             render:new
         end
     end
-    
+
+    def edit
+        
+    end
+
+    def update
+        
+        if @post.update(post_params)
+            redirect_to posts_path
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @post.destroy
+        redirect_to posts_path
+    end
+
     # post_paramsへの修飾
     private
 
     def post_params
         params.require(:post).permit(:title, :content)
+    end
+
+    def set_post
+        @post = Post.find(params[:id])
     end
 
 end
